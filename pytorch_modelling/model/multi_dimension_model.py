52665318beab4504b7c5d9104047440f294d329f
chiku-1436 <59862546+chiku-1436@users.noreply.github.com>
2021-05-08 18:26:46 +0530

    Add files via upload

---
+++ pytorch_modelling/model/multi_dimension_model.py
@@ -0,0 +1,89 @@
+import numpy as np
+import pandas as pd
+
+from models import *
+from model_training import *
+from utils.utils import *
+
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from torch.utils.data.sampler import SubsetRandomSampler
+
+
+# variable init
+batch_size = 32
+val_split = 0.3
+shuffle_dataset = True
+random_seed = 123
+visualization = True
+num_epochs = 4
+valid_loss_min = np.Inf
+model_name = 'resnet'
+num_classes = 2
+input_dim = 8
+
+
+# random image dataset for 8 dimension image
+class random_dataset(Dataset):
+    def __init__(self):
+        self.image = np.random.rand(5000,224,224,8)
+        self.labels = np.random.choice([0, 1], size=(5000,), p=[0.6,0.4])
+
+    def __getitem__(self, index):
+        image_1 = self.image[index]
+        labels_1 = self.labels[index]
+        image_2 = np.transpose(image_1, (2,1,0))
+        return image_2, labels_1
+    
+    def __len__(self):
+        return len(self.labels)
+
+
+
+# Creating training and validation dataset
+dataset = random_dataset()
+data_size = len(dataset)
+
+indices = list(range(data_size))
+split = int(np.floor(val_split * data_size))
+
+if shuffle_dataset :
+    np.random.seed(random_seed)
+    np.random.shuffle(indices)
+train_indices, val_indices = indices[split:], indices[:split]
+
+# Create train ad val data loaders:
+train_sampler = SubsetRandomSampler(train_indices)
+valid_sampler = SubsetRandomSampler(val_indices)
+
+train_loader = torch.utils.data.DataLoader(dataset,
+                                            batch_size=batch_size,
+                                            sampler=train_sampler,
+                                            num_workers=8,
+                                            pin_memory=True)
+val_loader = torch.utils.data.DataLoader(dataset,
+                                            batch_size=batch_size,
+                                            sampler=valid_sampler,
+                                            num_workers=8,
+                                            pin_memory=True)
+
+print('11')
+# resnet, vgg, densenet, alexnet
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model_ft, input_size = initialize_model(model_name, num_classes, 8, use_pretrained=True)
+
+# Put the model on the device:
+model_ft = model_ft.to(device)
+
+# define loss function
+criterion = nn.CrossEntropyLoss().to(device)
+optimizer = torch.optim.Adam(model_ft.parameters(), lr=0.0001)
+lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=num_epochs, eta_min=0.001)
+
+## model training
+## model_train function save model automatically as model_multi_dim.pth
+model_train(model_ft, optimizer, criterion, lr_scheduler, device, train_loader, val_loader, n_epochs=num_epochs).run()
---
+++ pytorch_modelling/utils/utils.py
@@ -0,0 +1,70 @@
+import torch
+import os
+import random as rn
+import numpy as np
+
+def set_seed():
+    os.environ['PYTHONHASHSEED'] = '0'                      
+    np.random.seed(123)
+    rn.seed(123)
+    torch.manual_seed(123)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(10)
+
+def binary_acc(y_pred, y_test):
+    y_pred_tag = torch.log_softmax(y_pred, dim = 1)
+    _, y_pred_tags = torch.max(y_pred_tag, dim = 1)
+    correct_results_sum = (y_pred_tags == y_test).sum().float()
+    acc = correct_results_sum/y_test.shape[0]
+    acc = torch.round(acc * 100)
+    return acc
+
+class AverageMeter(object):
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+class EarlyStopping:
+    def __init__(self, patience=7, verbose=False, delta=0, path='model_multi_dim.pth', trace_func=print):
+        self.patience = patience
+        self.verbose = verbose
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.val_loss_min = np.Inf
+        self.delta = delta
+        self.path = path
+        self.trace_func = trace_func
+    def __call__(self, val_loss, model):
+
+        score = -val_loss
+
+        if self.best_score is None:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+        elif score < self.best_score + self.delta:
+            self.counter += 1
+            self.trace_func(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+            self.counter = 0
+
+    def save_checkpoint(self, val_loss, model):
+        if self.verbose:
+            self.trace_func(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
+        torch.save(model, self.path)
+        self.val_loss_min = val_loss
